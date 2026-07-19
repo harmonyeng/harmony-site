a0959c33c809b42cd9c683395c6657e36d6519d7
harmonyeng <harmonyengineeringtn@gmail.com>
2026-07-19 10:48:28 +0300

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -10,6 +10,18 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: 'https://theharmonyeng.com/about',
       lastModified: new Date(),
     },
-    // תוסיפי כאן כל דף נוסף שיש לך (מאמרים, וכו')
+    {
+      url: 'https://theharmonyeng.com/sops/from-bottleneck-to-flow-how-to-find-your-home-s-chokepoint-and-fix-it',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://theharmonyeng.com/sops/running-a-home-is-running-a-business',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://theharmonyeng.com/sops/the-good-enough-manager-why-trying-to-be-perfect-is-quietly-wrecking-you',
+      lastModified: new Date(),
+    },
+    // כשתפרסמי מאמר חדש - תוסיפי כאן עוד בלוק כזה עם הכתובת שלו
   ]
 }
